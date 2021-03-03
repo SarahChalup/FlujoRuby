927ea4078bd6f04fcffc41f76ec88d433c338d43
SarahChalup <saritachalup@gmail.com>
2021-03-02 21:14:26 -0400

    Funcion CosmoDB y Queue

---
+++ FncConsumidor2.0/FncConsumidor2.0/Models/Data.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FncConsumidor2._0.Models
+{
+    class Data
+    {
+        [Key]
+        public string NameDevice { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime EventDate { get; set; }
+        [Required]
+        public string Event { get; set; }
+    }
+}
